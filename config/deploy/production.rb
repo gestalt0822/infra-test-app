37729cb6edd328b005d07466b730086ef7e95ac2
Aoki Daisuke <dice.k.aoki@gmail.com>
2017-07-23 15:17:51 +0900

    producton.rbを修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,13 @@
+# 環境名
+set :stage, :production
+
+# RAILS_ENVに設定する値
+set :rails_env, :production
+
+role :app, %w{ app@app1.infraoki.work }
+role :web, %w{ app@app1.infraoki.work }
+role :db,  %w{ app@app1.infraoki.work }
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
